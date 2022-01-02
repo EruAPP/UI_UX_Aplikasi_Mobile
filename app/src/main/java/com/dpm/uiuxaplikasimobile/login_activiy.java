ffb6f8b3718df18851e717c6b43aae67b049b41b
Adrian Perdana Putra <adrian.spembi@gmail.com>
2022-01-02 17:36:08 +0700

    P null

---
+++ app/src/main/java/com/dpm/uiuxaplikasimobile/login_activiy.java
@@ -0,0 +1,14 @@
+package com.dpm.uiuxaplikasimobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_activiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activiy);
+    }
+}
